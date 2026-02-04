0834440b4df2fee94fc3d2160fb3c6f931eafe25
mrJay090903 <jac2022-2256-23916@bicol-u.edu.ph>
2026-02-04 23:35:37 +0800

    fix

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,8 @@
 export const COURSES = [
-,
   'BS in Computer Science',
   'BS in Information System',
   'BS in Information Technology',
-  'BS in Information Technology (Animation)',
+  'BS in Information Technology (Animation)'
 ] as const
 
 export type Course = typeof COURSES[number]
